55d90b948566e59574a2a536d5a2f0c2635900a1
Michael Needleman <michael.needleman01@gmail.com>
2025-06-12 14:03:45 -0700

    Stub out about me

--- client/src/components/pages/AboutMe.tsx
+++ client/src/components/pages/AboutMe.tsx
@@ -2,9 +2,9 @@ import Col from "../flex/Col";
 
 const AboutMe = () => {
   return (
-    <Col className="items-center justify-center w-full h-full p-4">
-      <h1 className="text-4xl font-bold mb-4">About Me</h1>
-      <p className="text-lg text-gray-700 max-w-2xl text-center">
+    <Col className="items-center justify-center w-full h-full px-8">
+      <h1 className="text-3xl font-bold mb-4 text-component">About Me</h1>
+      <p className="text-lg text-comment max-w-2xl text-center">
         Put something here
       </p>
     </Col>
